930acb7d3cc1d1d44ad631bfe64f1d3b1bb1d1e7
Wendel Santos <wendel.santos@consultores.rnp.br>
2024-07-10 16:39:34 -0300

    first commit

---
+++ src/test/kotlin/tech/doit/app/school_manager/SchoolManagerApplicationTests.kt
@@ -0,0 +1,13 @@
+package tech.doit.app.school_manager
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SchoolManagerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
